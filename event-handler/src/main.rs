a75de1f7fb275b84fffe4f727ab0925157b32563
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-07-31 18:44:08 +0100

    Process all messages in the event
    A Lambda listening to an SQS queue can potentially receive multiple
    messages in a single event. Pass the message processing off to a
    separate function so the event handler can simply iterate through
    all messages.
    
    Also clean up the tests by adding a function to generate fixtures

--- event-handler/src/main.rs
+++ event-handler/src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use aws_lambda_events::event::sqs::SqsEvent;
+use aws_lambda_events::event::sqs::{ SqsEvent, SqsMessage};
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
 #[derive(Deserialize, Serialize)]
@@ -14,14 +14,29 @@ struct EventBody {
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/lambda-runtime/examples
 /// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<String, Error> {
-    // Extract some useful information from the request
 
-    // deserialise message body
-    let event_body = event.payload.records[0].body.as_ref().unwrap();
-    let body: EventBody = serde_json::from_str(event_body.as_str())?;
+fn process_message(event: SqsMessage) -> Result<String, Error> {
 
-    Ok(body.user_id)
+  // deserialise message body
+  let body: EventBody = {
+    let body = event.body.as_ref().unwrap();
+    serde_json::from_str(body.as_str())?
+  };
+  
+  Ok(body.user_id)
+}
+
+async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
+  // Lambdas listening to an SQS queue can be passed multiple messages in
+  // the same event. Iterate over each of them 
+  for message in event.payload.records {
+    match process_message(message) {
+      Ok(_) => {},
+      Err(_) => {}
+    };
+  }
+
+  Ok(())
 }
 
 #[tokio::main]
@@ -47,8 +62,32 @@ mod tests {
     Some(s.to_string())
   }
 
+  fn build_sqs_message(
+    message_id: &str,
+    receipt_handle: &str,
+    body: EventBody,
+    md5_of_body: &str,
+    md5_of_message_attributes: &str,
+    event_source_arn: &str,
+    event_source: &str,
+    aws_region: &str,
+  ) -> SqsMessage {
+    SqsMessage {
+      message_id: to_option_string(message_id),
+      receipt_handle: to_option_string(receipt_handle),
+      body: Some(serde_json::to_string(&body).unwrap()),
+      md5_of_body: to_option_string(md5_of_body),
+      md5_of_message_attributes: to_option_string(md5_of_message_attributes),
+      attributes: HashMap::new(),
+      message_attributes: HashMap::new(),
+      event_source_arn: to_option_string(event_source_arn),
+      event_source: to_option_string(event_source),
+      aws_region: to_option_string(aws_region) 
+    }
+  }
+
   #[tokio::test]
-  async fn test_my_function_handler_returns_user_id() {
+  async fn test_process_message_returns_user_id() {
 
     let body = EventBody {
       user_id: "user_id".to_string(),
@@ -56,27 +95,18 @@ mod tests {
       timestamp: "1659082455".to_string()
     };
 
-    let input = SqsEvent {
-      records: vec![
-        aws_lambda_events::event::sqs::SqsMessage {
-          message_id: to_option_string("message_id"),
-          receipt_handle: to_option_string("receipt_handle"),
-          body: Some(serde_json::to_string(&body).unwrap()),
-          md5_of_body: to_option_string("md5_of_body"),
-          md5_of_message_attributes: to_option_string("md5_of_message_attributes"),
-          attributes: HashMap::new(),
-          message_attributes: HashMap::new(),
-          event_source_arn: to_option_string("event_source_arn"),
-          event_source: to_option_string("aws:sqs"),
-          aws_region: to_option_string("eu-west-2") 
-        }
-      ]
-    };
-    let context = lambda_runtime::Context::default();
+    let message = build_sqs_message(
+      "message_id",
+      "receipt_handle",
+      body,
+      "md5_of_body",
+      "md5_of_message_attributes",
+      "event_source_arn",
+      "aws:sqs",
+      "eu-west-2" 
+    );
 
-    let event = LambdaEvent::new(input, context);
-
-    let response = function_handler(event).await.unwrap();
+    let response = process_message(message).unwrap();
 
     assert_eq!(response, "user_id".to_string());
   }
